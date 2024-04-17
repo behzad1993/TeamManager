9464475990c1004b06cb290df62c0b74a4a9a9e5
Behzad Karimi <karimi1993@live.de>
2024-04-17 17:46:04 +0200

    changes on mediatype

--- src/main/java/one/behzad/teammanager/models/fileDB/MediaType.java
+++ src/main/java/one/behzad/teammanager/models/fileDB/MediaType.java
@@ -1,15 +1,8 @@
 package one.behzad.teammanager.models.fileDB;
 
 public enum MediaType {
-
-    CLIP("clip"),
-    GIF("gif"),
-    IMAGE("image"),
-    VECTOR_GRAPHIC("vector_graphic");
-
-    private final String label;
-
-    MediaType(String label) {
-        this.label = label;
-    }
+    CLIP,
+    GIF,
+    IMAGE,
+    VECTOR_GRAPHIC;
 }
